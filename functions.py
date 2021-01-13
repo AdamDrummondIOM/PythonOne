194c09c36315b790a7b97b2a80a2f8e12403356b
Glitch (hello-webpage) <none>
2021-01-13 18:38:46 +0000

    🍲🚦 Checkpoint ./functions.py:27363384/480 ./returns.py:27363384/111

--- functions.py
+++ functions.py
@@ -1,3 +1,22 @@
-my_name 
+my_name = "Adam"
 
-def say_hello
+def say_hello(name):
+  print("Hi " + name)
+  
+say_hello(my_name)
+say_hello("Paul")
+say_hello("Lydia")
+say_hello("Aditya")
+
+def two_hellos(firstName, secondName):
+  print(firstName + " says hello to " + secondName)
+  
+two_hellos("Lily", "Elias")
+
+def multiply_numbers(num_one, num_two):
+  print(num_one * num_two)
+  
+  num_three = num_one * num_two
+  print(num_three)
+  
+multiply_numbers(6, 4)
--- returns.py
+++ returns.py
@@ -1,1 +1,2 @@
-
+def create_full_name(first_name, last_name):
+  full_name = first_name.upper() + " " + last_name.upper()
